7f6110ec61ee1fc3d16587295342861f9146c840
Mashrafi Ahnam Shafin <132218231+shafin90@users.noreply.github.com>
2023-09-11 18:18:17 -0700

    Update page.js

--- src/app/SinglaBlogPage/page.js
+++ src/app/SinglaBlogPage/page.js
@@ -4,8 +4,7 @@
 
 import React, { useContext, useEffect, useState } from "react";
 import { authContext } from "../layout";
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+
 
 
 const SingleBlogPage = () => {
@@ -24,16 +23,14 @@ const SingleBlogPage = () => {
         marginBottom : "100px"
     }
 
-    useEffect(() => {
-        AOS.init();
-      }, []);
+
 
 
     const renderContent = () => {
         // Check if currentBlog exists and has content
         if (currentBlog && currentBlog.content) {
             // Create a div with the sanitized HTML content
-            return <div data-aos="fade-up"  data-aos-duration="2000" style={style} dangerouslySetInnerHTML={{ __html: currentBlog.content }} />;
+            return <div  style={style} dangerouslySetInnerHTML={{ __html: currentBlog.content }} />;
         }
         
         // Return null or an appropriate message if there's no content
@@ -42,9 +39,9 @@ const SingleBlogPage = () => {
 
     return (
         <div >
-            <h1  data-aos-duration="1000" data-aos="flip-left" className="w-full text-center font-extrabold text-fuchsia-950 text-7xl mt-20 mb-4">{currentBlog?.title}</h1>
+            <h1  className="w-full text-center font-extrabold text-fuchsia-950 text-7xl mt-20 mb-4">{currentBlog?.title}</h1>
             <p className=" text-slate-500 mb-5 ms-32">Author Name: {currentBlog?.authorName}</p>
-            <img  data-aos-duration="2000" data-aos="zoom-in" className=" w-7/12 h-3/6 mx-auto mb-16" src={currentBlog?.image} alt={currentBlog?.title} />
+            <img   className=" w-7/12 h-3/6 mx-auto mb-16" src={currentBlog?.image} alt={currentBlog?.title} />
             {renderContent()}
         </div>
     );
